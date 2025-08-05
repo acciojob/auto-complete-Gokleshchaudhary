fb5d52edfd41f6bfd7b28389b0ea469f0f4e4f34
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-05 12:22:28 +0530

    Update App.cy.js

--- src/components/App.cy.js
+++ src/components/App.cy.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import App from './App' // ✅ Relative import from same folder
+import { mount } from 'cypress/react'
+
+describe('App', () => {
+  beforeEach(() => {
+    mount(<App />)
+  })
+
+  it('should display the search input', () => {
+    cy.get('.search').should('exist')
+  })
+})
 
